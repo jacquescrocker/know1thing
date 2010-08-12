b8605926f69538236dd93558ea4d768a7e1acc7e
Jacques Crocker <railsjedi@gmail.com>
2010-08-12 02:39:02 -0700

    fixing broken config

--- setup.rb
+++ setup.rb
@@ -1,11 +1,6 @@
 Frank.server.port = 4000
 
-Frank.static_folder = "public"
-
 if Frank.production?
   Frank.options.prefix = "/know1thing/"
-  Compass.configuration.http_images_path = "#{Frank.options.prefix}images"
-else
-  Compass.configuration.http_images_path = "/images"
 end
 
