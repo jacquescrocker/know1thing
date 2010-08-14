609ef15e9d4344b3833489ed9f6887890dbb7b6c
Jacques Crocker <railsjedi@gmail.com>
2010-08-13 21:55:19 -0700

    Made twitter share url even COOLER

--- helpers.rb
+++ helpers.rb
@@ -1,7 +1,25 @@
+require 'uri'
+
 module FrankHelpers
 
   def partial(*args)
     render_partial(*args)
+  end
+
+  def twitter_share(options = {})
+    %{
+      <a href="#{twitter_share_url(options)}" class="tweet">#{options[:label] || 'Tweet this'}</a>
+    }
+  end
+
+  def twitter_share_url(options = {})
+    base = "http://twitter.com/share?"
+    params = []
+    params << "url=#{URI.encode(options[:url])}"
+    params << "text=#{URI.encode(options[:text])}"
+    params << "via=#{URI.encode(options[:via])}" if options[:via]
+    params << "related=#{URI.encode(options[:related])}"
+    base+params.join("&")
   end
 
   def slidedeck
